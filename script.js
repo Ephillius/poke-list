608b8f5cc8495c80bf2839993701854cbb33eddf
Bernardo Ferreira <bernardo_m_ferreira@outlook.com>
2022-11-01 17:40:46 +0000

    Updated VUE2 CDN link

--- script.js
+++ script.js
@@ -24,40 +24,39 @@ new Vue({
             const self = this;
 
             axios.all([
-                    axios.get(urlPokemon),
-                    axios.get(urlPokedex)
-                ])
-                .then(axios.spread((pokemonRes, pokedexRes) => {
-                    const pokemonStats = pokemonRes.data;
-                    const pokedexEntry = pokedexRes.data;
+                axios.get(urlPokemon),
+                axios.get(urlPokedex)
+            ]).then(axios.spread((pokemonRes, pokedexRes) => {
+                const pokemonStats = pokemonRes.data;
+                const pokedexEntry = pokedexRes.data;
 
-                    console.log(pokemonStats);
-                    console.log(pokedexEntry);
+                console.log(pokemonStats);
+                console.log(pokedexEntry);
 
-                    //pokemon stats
-                    self.pokemonSprite = pokemonStats.sprites.front_default;
-                    self.name = pokemonStats.name;
-                    self.hp = pokemonStats.stats[5].base_stat;
-                    self.type1 = pokemonStats.types[0].type.name;
-                    if (pokemonStats.types.length == 2) {
-                        self.type2 = `/` + pokemonStats.types[1].type.name;
-                    } else {
-                        self.type2 = '';
+                //pokemon stats
+                self.pokemonSprite = pokemonStats.sprites.front_default;
+                self.name = pokemonStats.name.replace(pokemonStats.name.charAt(0), pokemonStats.name.charAt(0).toUpperCase());
+                self.hp = pokemonStats.stats[5].base_stat;
+                self.type1 = pokemonStats.types[0].type.name.replace(pokemonStats.types[0].type.name.charAt(0), pokemonStats.types[0].type.name.charAt(0).toUpperCase());
+                if (pokemonStats.types.length == 2) {
+                    self.type2 = `/` + pokemonStats.types[1].type.name.replace(pokemonStats.types[1].type.name.charAt(0), pokemonStats.types[1].type.name.charAt(0).toUpperCase());
+                } else {
+                    self.type2 = '';
+                }
+
+                //pokedex entry for the specific pokemon
+                const entriesList = pokedexEntry.flavor_text_entries;
+                for (i = 0; i < entriesList.length; i++) {
+                    if (entriesList[i].version.name == 'alpha-sapphire' && entriesList[i].language.name == 'en') {
+                        self.text = entriesList[i].flavor_text;
+                        break;
                     }
-
-                    //pokedex entry for the specific pokemon
-                    const entriesList = pokedexEntry.flavor_text_entries;
-                    for (i = 0; i < entriesList.length; i++) {
-                        if (entriesList[i].version.name == 'alpha-sapphire' && entriesList[i].language.name == 'en') {
-                            self.text = entriesList[i].flavor_text;
-                            break;
-                        }
-                    }
-                }))
-                .catch(axios.spread((pokemonError, pokedexError) => {
-                    console.log(pokemonError);
-                    console.log(pokedexError);
-                }))
+                }
+            }))
+            .catch(axios.spread((pokemonError, pokedexError) => {
+                console.log(pokemonError);
+                console.log(pokedexError);
+            }))
         }
     }
 });
